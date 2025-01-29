5cc1ff36c62b49fd2f4256870e548361a2c282e8
dirasanjayawardana <dirawardana.dw@gmail.com>
2025-01-29 13:41:53 +0700

    update

--- src/dirapp/CListApp.java
+++ src/dirapp/CListApp.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ListApp {
+public class CListApp {
     public static void main(String[] args){
 
         // List adalah struktur data collection yang memiliki sifat:
